308cae89345512ff99432a26c2977b8640d9c2fd
Patrick (Pololu) <dev-patrick@pololu.com>
2023-04-04 17:14:02 -0700

    Patrick's initial attempt at modifying LPS library to support new LPS22DF chip.  Updates include: - adding 22DF registers - updating registers to be device specific as necessary - adding translation support for different-address registers - updating temperature conversion functions as needed since the conversion for the 22DF is different from the previous LPS sensors

--- LPS.h
+++ LPS.h
@@ -6,7 +6,7 @@
 class LPS
 {
   public:
-    enum deviceType { device_331AP, device_25H, device_22DF, device_auto };
+    enum deviceType { device_331AP, device_25H, device_auto };
     enum sa0State { sa0_low, sa0_high, sa0_auto };
 
     // register addresses
@@ -16,66 +16,103 @@ class LPS
     enum regAddr
     {
       REF_P_XL                = 0x08,
-      REF_P_L                 = 0x09,
-      REF_P_H                 = 0x0A,
-
+	  
+	  IF_CTRL                 = 0x0E, // 22DF
+                              
       WHO_AM_I                = 0x0F,
-
+                              
       RES_CONF                = 0x10,
-
-      CTRL_REG2               = 0x21,
-      CTRL_REG3               = 0x22,
-      CTRL_REG4               = 0x23, // 25H
-
+	  
+	  FIFO_WTM                = 0x15, // 22DF
+	  
+	  I3C_IF_CTRL_ADD         = 0x19, // 22DF
+	  
+	  FIFO_STATUS1            = 0x25, // 22DF
+	  FIFO_STATUS2            = 0x26, // 22DF              
       STATUS_REG              = 0x27,
-
+                            
       PRESS_OUT_XL            = 0x28,
       PRESS_OUT_L             = 0x29,
       PRESS_OUT_H             = 0x2A,
 
       TEMP_OUT_L              = 0x2B,
       TEMP_OUT_H              = 0x2C,
-
-      FIFO_CTRL               = 0x2E, // 25H
+      
       FIFO_STATUS             = 0x2F, // 25H
-
+      
       AMP_CTRL                = 0x30, // 331AP
-
+      
       RPDS_L                  = 0x39, // 25H
       RPDS_H                  = 0x3A, // 25H
-
+      
       DELTA_PRESS_XL          = 0x3C, // 331AP
       DELTA_PRESS_L           = 0x3D, // 331AP
       DELTA_PRESS_H           = 0x3E, // 331AP
-
-
+	  
+	  FIFO_DATA_OUT_PRESS_XL  = 0x78, // 22DF
+	  FIFO_DATA_OUT_PRESS_L   = 0x79, // 22DF
+	  FIFO_DATA_OUT_PRESS_H   = 0x7A, // 22DF
+      
+      
       // dummy addresses for registers in different locations on different devices;
       // the library translates these based on device type
       // value with sign flipped is used as index into translated_regs array
-
-      INTERRUPT_CFG    = -1,
-      INT_SOURCE       = -2,
-      THS_P_L          = -3,
-      THS_P_H          = -4,
-      CTRL_REG1        = -5,
+    
+	  REF_P_L          = -1,
+	  REF_P_H          = -2,
+	  CTRL_REG1        = -3,
+	  CTRL_REG2        = -4,
+	  CTRL_REG3        = -5,
+	  CTRL_REG4        = -6, 
+	  INTERRUPT_CFG    = -7,
+      INT_SOURCE       = -8,
+	  FIFO_CTRL        = -9,
+      THS_P_L          = -10,
+      THS_P_H          = -11,
+	  RPDS_L           = -12,
+	  RPDS_H           = -13,
       // update dummy_reg_count if registers are added here!
-
-
+      
+      
       // device-specific register addresses
-
-      LPS331AP_CTRL_REG1      = 0x20,
-      LPS331AP_INTERRUPT_CFG  = 0x23,
+      
+      LPS331AP_REF_P_L        = 0x09,
+	  LPS331AP_REF_P_H        = 0x0A,
+	  LPS331AP_CTRL_REG1      = 0x20,
+	  LPS331AP_CTRL_REG2      = 0x21,
+	  LPS331AP_CTRL_REG3      = 0x22,
+	  LPS331AP_INTERRUPT_CFG  = 0x23,
       LPS331AP_INT_SOURCE     = 0x24,
       LPS331AP_THS_P_L        = 0x25,
       LPS331AP_THS_P_H        = 0x26,
-
+      
+	  LPS25H_REF_P_L          = 0x09,
+	  LPS25H_REF_P_H          = 0x0A,
       LPS25H_CTRL_REG1        = 0x20,
-      LPS25H_INTERRUPT_CFG    = 0x24,
+	  LPS25H_CTRL_REG2        = 0x21,
+	  LPS25H_CTRL_REG3        = 0x22,
+	  LPS25H_CTRL_REG4        = 0x23,
+	  LPS25H_INTERRUPT_CFG    = 0x24,
       LPS25H_INT_SOURCE       = 0x25,
+	  LPS25H_FIFO_CTRL        = 0x2E,
       LPS25H_THS_P_L          = 0x30,
       LPS25H_THS_P_H          = 0x31,
-
-      LPS22DF_CTRL_REG1       = 0x10,
+	  LPS25H_RPDS_L           = 0x39,
+	  LPS25H_RPDS_H           = 0x3A,
+	  
+	  LPS22DF__INTERRUPT_CFG  = 0x0B,
+	  LPS22DF__THIS_P_L       = 0x0C,
+	  LPS22DF__THIS_P_H       = 0x0D,
+	  LPS22DF_CTRL_REG1       = 0x10,
+	  LPS22DF_CTRL_REG2       = 0x11,
+	  LPS22DF_CTRL_REG3       = 0x12,
+	  LPS22DF_CTRL_REG4       = 0x13,
+	  LPS22DF_FIFO_CTRL       = 0x14,
+	  LPS22DF_REF_P_L         = 0x16,
+	  LPS22DF_REF_P_H         = 0x17,
+	  LPS22DF_RPDS_L          = 0x1A,
+	  LPS22DF_RPDS_H          = 0x1B,
+	  LPS22DF_INT_SOURCE      = 0x24,
     };
 
     LPS(void);
@@ -100,10 +137,10 @@ class LPS
     static float pressureToAltitudeFeet(float pressure_inHg, float altimeter_setting_inHg = 29.9213);
 
   private:
-    deviceType _device; // chip type (331AP or 25H)
+    deviceType _device; // chip type (331AP, 25H, 22DF)
     byte address;
-
-    static const int dummy_reg_count = 5;
+    
+    static const int dummy_reg_count = 4;
     regAddr translated_regs[dummy_reg_count + 1]; // index 0 not used
 
     bool detectDeviceAndAddress(deviceType device, sa0State sa0);
